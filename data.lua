917d2865d3bfd624e97dc6e09afc618f4f163b8c
paradosi <mail@paradosi.dev>
2026-02-12 08:27:36 -0500

    Initial TBC Classic Anniversary port
    Port of FloAspectBar (by Floraline, MPL 2.0) to TBC Classic Anniversary (Interface 20505).
    
    - Added Aspect of the Viper to spell list (7 aspects total)
    - Stripped all retail/mainline code paths
    - Fixed BackdropTemplateMixin for modern client engine
    - Replaced SmallActionButtonTemplate with TBC-compatible button template
    - Guarded COOLDOWN_TYPE_NORMAL for TBC API compatibility
    - Removed invalid LEARNED_SPELL_IN_TAB event

---
+++ data.lua
@@ -0,0 +1,15 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this file,
+-- You can obtain one at http://mozilla.org/MPL/2.0/.
+
+FLO_ASPECT_SPELLS = {
+	["HUNTER"] = {
+		{ id = 13163 }, -- Aspect of the Monkey
+		{ id = 13165 }, -- Aspect of the Hawk
+		{ id = 34074 }, -- Aspect of the Viper
+		{ id = 5118 },  -- Aspect of the Cheetah
+		{ id = 13161 }, -- Aspect of the Beast
+		{ id = 13159 }, -- Aspect of the Pack
+		{ id = 20043 }, -- Aspect of the Wild
+	}
+};
